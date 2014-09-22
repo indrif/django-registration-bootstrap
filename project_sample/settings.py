965c205c4068e15973c42d96c8c4e2381a99d124
Martín Gaitán <gaitan@gmail.com>
2014-09-22 00:11:54 -0300

    renamed app in the example project

--- project_sample/settings.py
+++ project_sample/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'app_sample',
+    'registration_bootstrap3',
 )
 
 MIDDLEWARE_CLASSES = (
